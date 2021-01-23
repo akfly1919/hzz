c6a7d0621d9ddacad28da17f75706ab78616401c
孟亮亮 <mengliangliang@ipaynow.cn>
2021-01-23 12:33:34 +0800

    提交代码

--- src/main/java/com/akfly/hzz/HzzApplication.java
+++ src/main/java/com/akfly/hzz/HzzApplication.java
@@ -4,12 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @MapperScan("com.akfly.hzz.mapper")
 @EnableCaching
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class HzzApplication {
 
 	public static void main(String[] args) {
