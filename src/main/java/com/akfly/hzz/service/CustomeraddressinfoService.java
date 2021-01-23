8530c28856a57136bb96e14dd8ecf9eaaf4076fe
xuhuamei <xuhm61@chinaunicom.cn>
2021-01-23 20:36:15 +0800

    增加swagger

--- src/main/java/com/akfly/hzz/service/CustomeraddressinfoService.java
+++ src/main/java/com/akfly/hzz/service/CustomeraddressinfoService.java
@@ -1,7 +1,10 @@
 package com.akfly.hzz.service;
 
+import com.akfly.hzz.exception.HzzBizException;
 import com.akfly.hzz.vo.CustomeraddressinfoVo;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-01-18
  */
 public interface CustomeraddressinfoService extends IService<CustomeraddressinfoVo> {
-
+    public List<CustomeraddressinfoVo> getUserInfoById(String cbiId) throws HzzBizException;
 }
